8b7662afb09fdb8a91d2e46d02d5fad1eea584ec
Karthik Vellur <karthikvn23@gmail.com>
2021-06-13 12:28:50 +0530

    created Room Database

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,42 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    // "companion" Allows clients to access methods for creating & getting databases without instantation
+    companion object{
+
+        // "@Volatile" Helps to ensure value of INSTANCE is up to-date and all threads have the same value
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context) : SleepDatabase {
+            synchronized(this){
+                // Local variable
+                var instance = INSTANCE
+
+                if(instance == null){
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+}
